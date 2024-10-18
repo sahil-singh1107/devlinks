020c7164b9ab32af2cfe51282ae5b715cd28423d
sahil-singh1107 <sahil@LAPTOP-TNOQMCGR>
2024-10-18 15:31:41 +0530

    connected to express server and mongodb server

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const port = 3000
+const dotenv = require('dotenv');
+dotenv.config();
+const dbUrl = process.env.DATABASE_URL;
+
+app.use(express.json())
+
+async function main() {
+    try {
+      await mongoose.connect(dbUrl);
+      console.log('Connected to MongoDB');
+      app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+    } catch (error) {
+      console.error('Error connecting to the database', error);
+      process.exit(1); 
+    }
+  }
+  
+  main()
